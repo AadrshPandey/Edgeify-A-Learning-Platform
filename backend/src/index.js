766949d66d0c26129a36231cebd9a8d0b5ff1ab1
Aadrsh Pandey <ap601130@gmail.com>
2026-05-28 00:39:27 +0530

    Async Handler and Error Handler utils

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,3 +6,15 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+    app.on("error", (error)=>{
+        console.log("Server Error : ", error);
+        throw error;
+    })
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log("Server is running on PORT : ", process.env.PORT);
+    })
+})
+.catch((error)=>{
+    console.log("MongoDB connection failed !! ", error);
+})
